7f196d432e2c7462fc97f1e04b8b5ff8776333da
Kulesh Aleksandr <53787083+6mahdapihka9@users.noreply.github.com>
2021-10-05 12:28:25 +0300

    minesweeper added in app module pathes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,10 +32,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { environment } from "../environments/environment";
 
 const appRoutes: Routes = [
-  {path: 'game', component: GameComponent, data: { animation: "game" }},
-  {path: 'statistics', component: StatisticsComponent, data: { animation: "statistics" }},
-  {path: 'records', component: RecordsComponent, data: { animation: "records" }},
-  {path: '**', component: GameComponent, data: { animation: "game" }}
+  {path: 'MineSweeper/game', component: GameComponent, data: { animation: "game" }},
+  {path: 'MineSweeper/statistics', component: StatisticsComponent, data: { animation: "statistics" }},
+  {path: 'MineSweeper/records', component: RecordsComponent, data: { animation: "records" }},
+  {path: 'MineSweeper/**', component: GameComponent, data: { animation: "game" }}
 ]
 
 @NgModule({
